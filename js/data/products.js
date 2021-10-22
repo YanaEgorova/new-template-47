d7533536e8f260aa13a295369d3bebe72648d66e
Yana <yanaegorova69.3@gmail.com>
2021-10-23 00:12:37 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,298 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Puppy Pads',
+        price: 19.99,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Includes 100, Regular Size Pads.',
+            },
+            {
+                text: 'Super-absorbent core that turns liquid to gel upon contact.',
+            },
+            {
+                text: 'Leak-proof with plastic lining to prevent damage to floors.',
+            },
+            {
+                text: 'Quick-dry surface with built-in attractant.',
+            },
+            {
+                text: 'Pad Dimensions: Each pad measures 22x22 inches (LxW), with the center pad measuring 19x19 inches (LxW). The pad has a 1.5 inch plastic border on all sides to prevent overflow.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Grass Puppy Pad',
+        price: 24.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Dog Training Pads',
+                text: 'Fake grass pee pads are essential dog housebreaking supplies that help to make potty training your puppy or small pet, a bit easier.',
+            },
+            {
+                title: 'Artificial Grass Puppy Pad',
+                text: 'Puppy pads are also perfect for owners that work long hours or live in apartments by providing a spot for your pets to relieve themselves indoors.',
+            },
+            {
+                title: 'Three Layer System',
+                text: 'This pet pad encompasses a three-layer system that makes clean up effortless; The mat sits on top of the plastic insert that locks in and allows liquid to drain into the durable collection tray.',
+            },
+            {
+                title: 'Easy Clean-Up',
+                text: 'Cleaning the pet bathroom mat is quick and easy. Clean-up consists only of rinsing the three layers with soapy water.',
+            },
+            {
+                title: 'Product Details',
+                text: 'Materials: Synthetic Plastic (Grass Mat) and Hard Plastic (Tray and Insert). Dimensions: 20” x 25” x 1.25”',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Pet Feeder',
+        price: 35.32,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Schedule Meals And Treats',
+                text: 'One meal is ready to eat as soon as you fill the tray with your pet’s food, and you can schedule the other 4 meals (or treats) to be served at a later time.',
+            },
+            {
+                title: 'Food Capacity',
+                text: 'Each compartment holds up to 1 cup of dry or semi-moist pet food; feeder is best for cats and small to medium sized dogs.',
+            },
+            {
+                title: 'Great For Fast Eaters',
+                text: 'If your cat or dog tends to eat too quickly, use this feeder to break their meals up into smaller portions.',
+            },
+            {
+                title: 'Multiple Options',
+                text: 'Press the “feed now” button to give your pet an extra meal; Suspend feeding option stops the feeder from serving the next meal without losing the schedule.',
+            },
+            {
+                title: 'Help Manage Pet’s Weight',
+                text: 'Schedule smaller meals throughout the day to help keep your best friend healthy.',
+            },
+            {
+                title: 'Dishwasher Safe',
+                text: 'Place the removable BPA-free tray in the top rack of the dishwasher for easy cleanup.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Cat House',
+        price: 39.99,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'Penthouse was specially designed to be safe for your pets and safe for our planet!',
+            },
+            {
+                text: 'Our cardboard pet houses reduce stress and anxiety, which is why cats are naturally drawn to our eco-friendly cardboard cat houses.',
+            },
+            {
+                text: 'SUSTAINABLE, lightweight and sturdy, with multiple scratch boards, your feline friend will be clawing for more! Your feline friend will feel like the next great cats by of their very own Penthouse!',
+            },
+            {
+                text: 'FOR pets only: keep in a dry area. Do not rinse or wash, do not leave around heat, The cardboard will warp and soften.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png', './img/img4-4.png', './img/img4-5.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Dog Bed',
+        price: 43.98,
+        image: './img/img5.png',
+        description: [
+            {
+                title: 'Luxurious Vegan Fur',
+                text: 'Cozy, self-warming faux shag fur is reminiscent of a mother\'s fur coat which aids in deeper and more restful sleep.',
+            },
+            {
+                title: 'Versatile Design And Maintenance',
+                text: 'Conveniently designed with zippers for replacment and washing. Keep an extra cover on hand so your furry pals will never go without a clean bed.',
+            },
+            {
+                title: 'Pet-Safe Materials',
+                text: 'Tested through Consumer Product Safety Commission certified labs to ensure safety for you and your pets. Free from: flame retardants, formaldehyde, skin irritating chemicals, harsh dyes.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Cat Water Fountain',
+        price: 49.95,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Circulating drinking water is naturally appealing to your pet.',
+            },
+            {
+                text: 'Replaceable charcoal filter for clean and pure, healthy water.',
+            },
+            {
+                text: 'Dishwasher Safe. Refer the user guide below for Pum Trouble shooting steps and Installation steps.',
+            },
+            {
+                text: '128 oz capacity. refer Troubleshooting guide for Product instruction.',
+            },
+            {
+                text: 'Modern design fits in any home décor.',
+            },
+            {
+                text: 'Maintenance Guide - Pump needs to be cleaned out monthly to make sure it stays in good working order. More details given in user manual.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'Cat Enclosure',
+        price: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: 'Outdoor net enclosure for cats; great for decks, patios, and balconies.',
+            },
+            {
+                text: 'Self standing, open-net construction for maximum visibility and ventilation.',
+            },
+            {
+                text: 'Completely enclosed with 2 doors; solid steel wickets and rip-stop netting.',
+            },
+            {
+                text: 'Weather resistant for all seasons; handy travel and storage bag included.',
+            },
+            {
+                text: 'Can be teamed up with Kittywalk lawn version and other components.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Wall Mounted Shelves',
+        price: 79.99,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Hand-crafted beautiful, large, and sleek design.',
+            },
+            {
+                text: 'Perfect play structure to bring out the acrobat in your cat.',
+            },
+            {
+                text: 'Gives cats an outlet for their climbing needs.',
+            },
+            {
+                text: 'Fabric can be easily removed for washing.',
+            },
+            {
+                text: 'Each hammock tested to hold 62lbs and each shelf is tested to hold 85lbs.',
+            },
+        ],
+        images: ['./img/img8-1.jpg', './img/img8-2.jpg'],
+    },
+    {
+        id: 'product-9',
+        name: 'Closed Aquatic Ecosystem',
+        price: 84.41,
+        image: './img/img9.png',
+        description: [
+            {
+                text: 'No maintenance aquarium, a complete miniature world.',
+            },
+            {
+                text: 'World\'s first totally enclosed ecosystem utilizing NASA technology.',
+            },
+            {
+                text: 'Unique gift item, perfect blend of science and art.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.png', './img/img9-3.png'],
+    },
+    {
+        id: 'product-10',
+        name: 'Hoof Grooming Care Tool',
+        price: 89.99,
+        image: './img/img10.png',
+        description: [
+            {
+                text: '15" Clincher.',
+            },
+            {
+                text: 'Well assorted farrier tool kit includes a free roll-up bag for easy transport. Tools Included in the kit are listed below.',
+            },
+            {
+                text: '(1) Nail Puller, (1) Nipper, (1) Clincher, (1) Rasp, (1) Rasp Wood Handle, (1) Hammer, (1) Buffer, (1) Farrier Hoof Knife.',
+            },
+        ],
+        images: ['./img/img10-1.png'],
+    },
+    {
+        id: 'product-11',
+        name: 'Dog Device',
+        price: 119.98,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Long & Short Contacts',
+                text: 'Includes two lengths of stainless steel, insulated contact points for reliable performance in wet conditions and extended comfort for any coat length.',
+            },
+            {
+                title: 'Barklimiter',
+                text: 'Built-in BarkLimiter with Advanced Bark Correction Technology.',
+            },
+            {
+                title: 'Range / Frequency',
+                text: '1 mile range with 27MHz radio frequency for PRO 70 and PRO 550.',
+            },
+            {
+                title: 'Includes',
+                text: 'PT 10 dog device, 3/4” blue collar strap, AC adapter, charging clip, contact point set w/ wrench, manual.',
+            },
+            {
+                text: 'Battery life- 60 hours. Collar compatibility- 0.75 Inch (1.9 cm).',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Pet Dryer',
+        price: 148.00,
+        image: './img/img12.png',
+        description: [
+            {
+                title: 'Easy to Use Design',
+                text: 'The unit fits on an adjustable height stand that also rolls, allowing you to move it around your salon wherever it’s needed.',
+            },
+            {
+                title: 'Adjustable Height',
+                text: 'The pet dryer fits on an adjustable stand that ranges between 35 and 60 inches in height.',
+            },
+            {
+                title: 'Three Nozzles',
+                text: 'Select from different nozzles to use with the grooming dryer, depending on the type of fur coat you’re drying.',
+            },
+            {
+                title: 'Blue and Black Dog Dryer',
+                text: 'This particular dryer is offered in blue and black colors, and purple/black and white/black designs are available separately.',
+            },
+            {
+                title: 'Powerful Pet Grooming Tool',
+                text: 'The unit is able to deliver 168.3 cubic feet per minute of air volume.',
+            },
+        ],
+        images: ['./img/img12-1.png','./img/img12-2.png'],
+    },
+]
